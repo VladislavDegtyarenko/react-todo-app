dc1be3a6e07928b2d71aa1781ffbde3c3261d90e
VladislavDegtyarenko <vladislavdegtyarenko@gmail.com>
2023-07-17 18:02:40 +0300

    new Footer text

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import "./Footer.css";
 
 export default function Footer() {
-   return (
-      <footer className="footer">
-         <small className="footer__copyright">
-            &#169; All Rights Reserved.{" "}
-            <a href="https://github.com/VladislavDegtyarenko/" target="_blank">
-               Vladyslav Dihtiarenko
-            </a>
-         </small>
-      </footer>
-   );
+  return (
+    <footer className="footer">
+      <small className="footer__copyright">
+        © Copyright {new Date().getFullYear()} |&nbsp;
+        <a href="https://vd-developer.vercel.app/" target="_blank">
+          Vladyslav Dihtiarenko
+        </a>
+        &nbsp;| All Rights Reserved
+      </small>
+    </footer>
+  );
 }
